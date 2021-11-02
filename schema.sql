96c2470d1af6533d1bf2c7a3f9b809aa8329dc68
leblabac <87709841+leblabac@users.noreply.github.com>
2021-11-02 18:40:56 -0500

    update to schema.sql

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-    emp_no INT NOT NULL,
+     emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -23,7 +23,7 @@ dept_no VARCHAR(4) NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -45,11 +45,12 @@ CREATE TABLE titles (
 );
 
 CREATE TABLE dept_emp (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no)
-);
+    PRIMARY KEY (emp_no, dept_no,from_date)
+ );
+   
