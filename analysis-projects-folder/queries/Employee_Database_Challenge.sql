7c3c410ca593fef6c2bd0209b57fd1048d2b6cf6
leblabac <87709841+leblabac@users.noreply.github.com>
2021-11-07 17:57:02 -0600

    updates to challenge query sheet

--- analysis-projects-folder/queries/Employee_Database_Challenge.sql
+++ analysis-projects-folder/queries/Employee_Database_Challenge.sql
@@ -10,12 +10,12 @@ INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND t.to_date = '9999-01-01'
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by e.emp_no;
 -- Review data
 SELECT * FROM retirement_titles
 
--- Use Dictinct with Orderby to remove duplicate rows
+-- Create a Unique Titles table that contains the employee number, first and last name, and most recent title
 SELECT DISTINCT ON (emp_no)
 emp_no,
 first_name,
@@ -23,19 +23,18 @@ last_name,
 title
 INTO unique_titles
 FROM retirement_titles
-ORDER BY emp_no, to_date DESC;
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
 --Review for data
 SELECT * FROM unique_titles
 
-
-
-
-
-
-
-
-
-
+--Create a Retiring Titles table that contains the number of titles filled by employees who are retiring.
+SELECT COUNT(u.emp_no),
+u.title
+INTO retiring_titles
+FROM unique_titles as u
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
 
 -- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
 -- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
@@ -55,30 +54,3 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
 
-
-DROP TABLE mentorship_eligibility
-
-SELECT DISTINCT ON(e.emp_no) e.emp_no, 
-    e.first_name, 
-    e.last_name, 
-    e.birth_date,
-    de.from_date,
-    de.to_date,
-    t.title
-INTO mentorship_eligibilty
-FROM employees as e
-Left outer Join dept_emp as de
-ON (e.emp_no = de.emp_no)
-Left outer Join titles as t
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
---Review Data
-SELECT * FROM mentorship_eligibilty
-
-
-
-
-\DROP TABLE unique_titles
-
-SELECT * FROM retirement_titles
