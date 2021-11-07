5d9d791dd589dd0336066cf8e9be173a5a7cb6d4
leblabac <87709841+leblabac@users.noreply.github.com>
2021-11-07 12:47:40 -0600

    employee_challenge updates

---
+++ Employee_Database_Challenge.sql
@@ -0,0 +1,84 @@
+-- DELIVERABLE 1: The Number of Retiring Employees by Title
+--Create a Retirement Titles table for employees who are born between January 1, 1952 and December 31, 1955
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND t.to_date = '9999-01-01'
+order by e.emp_no;
+-- Review data
+SELECT * FROM retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no)
+emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+--Review for data
+SELECT * FROM unique_titles
+
+
+
+
+
+
+
+
+
+
+
+-- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+-- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+
+DROP TABLE mentorship_eligibility
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+--Review Data
+SELECT * FROM mentorship_eligibilty
+
+
+
+
+\DROP TABLE unique_titles
+
+SELECT * FROM retirement_titles
